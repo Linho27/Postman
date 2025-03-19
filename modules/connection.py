9dc29d65362c237bbcab49c065274c28e61db501
Linho <69125386+Linho27@users.noreply.github.com>
2025-03-19 12:27:46 +0000

    Update connection.py

--- modules/connection.py
+++ modules/connection.py
@@ -1,31 +1,21 @@
-#LEDs controlling module
+#API connection module
 
 # Libraries import
 import requests
-from flask import Flask, jsonify, request
 
+def obter_pokemon(nome_ou_id):
+    url = f"https://pokeapi.co/api/v2/pokemon/{nome_ou_id}"
+    resposta = requests.get(url)
+    
+    if resposta.status_code == 200:
+        return resposta.json()
+    else:
+        print("Erro ao obter dados do Pokémon.")
+        return None
 
-#This module will include necessary stuff for connection (maybe also comparison) with the mts
-
-app = Flask(__name__) 
-
-@app.route('/')
-def home():
-    return "Hello, World!"
-
-@app.route('/data', methods=['GET'])
-def get_data():
-    data = {
-        "id": 1,
-        "name": "Example",
-        "description": "This is an example data."
-    }
-    return jsonify(data)
-
-@app.route('/data', methods=['POST'])
-def create_data():
-    new_data = request.get_json()
-    return jsonify(new_data), 201
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# Exemplo de utilização
+pokemon = obter_pokemon("pikachu")
+if pokemon:
+    print(f"Nome: {pokemon['name']}")
+    print(f"Altura: {pokemon['height']} cm")
+    print(f"Peso: {pokemon['weight']} kg")
