5978c4cc3c90a0ad3f87a042538677dfb19db2a8
Linho <69125386+Linho27@users.noreply.github.com>
2025-03-19 10:01:13 +0000

    Update fan.py

--- modules/fan.py
+++ modules/fan.py
@@ -7,12 +7,12 @@ from time import sleep
 
 # Pin & MaxTemp definition
 fanPin = 17
-tempLimit = 40
+tempLimitMax = 40
 
 # Global fanStatus variable for tracking the fan state
 fanStatus = False  # Start fan status as off
 
-# Check temperature function
+# Check temperature functions
 def check_temp():
     # Pins setup
     GPIO.setmode(GPIO.BCM)
@@ -26,13 +26,13 @@ def check_temp():
         print(f"Temperatura atual: {cpuTemp}°C")  # Print said temp for Debug purposes
         
         # If the CPU temperature is higher than the limit and the fan is off, turn it on
-        if cpuTemp > tempLimit and not fanStatus:
+        if cpuTemp > tempLimitMax and not fanStatus:
             print("Temperatura alta! Ligando ventoinha...")
             GPIO.output(fanPin, GPIO.HIGH)
             fanStatus = True  # Update fanStatus to True when the fan is turned on
             
         # If the CPU temperature is lower than the limit and the fan is on, turn it off
-        elif cpuTemp <= tempLimit and fanStatus:
+        elif cpuTemp <= tempLimitMax and fanStatus:
             print("Temperatura dentro do limite. Desligando ventoinha...")
             GPIO.output(fanPin, GPIO.LOW)
             fanStatus = False  # Update fanStatus to False when the fan is turned off
