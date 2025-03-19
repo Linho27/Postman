325764f29a8f5d256ec32b85ca6c974ee737ce54
kiko192005 <franciscogoncalves192005@gmail.com>
2025-03-19 11:44:50 +0000

    a

--- modules/connection.py
+++ modules/connection.py
@@ -1,5 +1,3 @@
-pip install Flask               #Web server
-
 #LEDs controlling module
 
 # Libraries import
@@ -17,7 +15,7 @@ def home():
 
 @app.route('/data', methods=['GET'])
 def get_data():
-   data = {
+    data = {
         "id": 1,
         "name": "Example",
         "description": "This is an example data."
@@ -25,9 +23,9 @@ def get_data():
     return jsonify(data)
 
 @app.route('/data', methods=['POST'])
-def create_data ():
-   new_data = request.get_json()
-return jsonify(new_data), 201
+def create_data():
+    new_data = request.get_json()
+    return jsonify(new_data), 201
 
-if__name__ == '__main__':
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
