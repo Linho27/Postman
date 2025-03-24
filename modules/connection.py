fc31bf14d6aa7ce82586f0ef6b5a45721b5a7686
Linho <69125386+Linho27@users.noreply.github.com>
2025-03-24 14:24:49 +0000

    Fixed isOccupied function always returning false

--- modules/connection.py
+++ modules/connection.py
@@ -1,43 +1,35 @@
-#API connection module
+# API connection module
 
 # Libraries import
 import requests
 
-#Define API link
+# Define API link
 linkAPI = "http://127.0.0.1:5000"
-#End points:
+# End points:
 #   /everyPlate
 #   /status/{pos}
 #   /toggle/{pos}
 
-#Function to get the position of a specific plate
+# Function to get the position of a specific plate
 def getPos(id):
     url = linkAPI + f'/everyPlate'
     try:
-        # Get request to get info of every plate
         response = requests.get(url)
-        
-        # Verify if answer was successful
         if response.status_code == 200:
             plates = response.json()
-            
-            # Search plate by its id
             for position, data in plates.items():
-                if data["numero_serie"] == id:
-                    return position  # Return plate position
+                if data["id"] == id:
+                    return position
             return f"Peça com código de série {id} não encontrada."
         else:
             return f"Erro ao obter dados do servidor. Código de status: {response.status_code}"
-    
     except requests.exceptions.RequestException as e:
         return f"Erro na requisição: {e}"
 
-#Function to toggle position status
+# Function to toggle position status
 def togglePos(pos):
-    #Check if position is within limits
     if not (1 <= pos <= 12):
         return "Número inválido. Deve estar entre 1 e 12."
-
     url = linkAPI + f"/toggle/{pos}"
     try:
         response = requests.post(url)
@@ -47,19 +39,15 @@ def togglePos(pos):
     except requests.exceptions.RequestException as e:
         return f"Erro na requisição: {e}"
 
-#Function to check if a specific position is occupied    
+# Function to check if a specific position is occupied    
 def isOccupied(pos):
     url = linkAPI + f"/status/{pos}"    
     try:
-        # Send GET request to the API
         response = requests.get(url)
-        
-        # Verify if answer was successful
         if response.status_code == 200:
             state = response.json()
-            
-            # Check if position is occupied
-            return state.get(str(pos)) == "ocupado"
+            pos_data = state.get(str(pos), {})
+            return pos_data.get("estado") == "ocupado"
         else:
             print(f"Erro na requisição: {response.status_code}")
             return False
