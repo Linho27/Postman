531123933c141524d1a11ea7a5f30bab93d968ca
Linho <69125386+Linho27@users.noreply.github.com>
2025-05-13 14:45:06 +0100

    Create mainteste.py

---
+++ mainteste.py
@@ -0,0 +1,11 @@
+import board
+import neopixel
+
+# Número de LEDs na fita
+LED_COUNT = 60  # Substitua pelo número real de LEDs
+
+# Inicializa o objeto NeoPixel
+pixels = neopixel.NeoPixel(board.D18, LED_COUNT, auto_write=True)
+
+# Define todos os LEDs para branco (255, 255, 255)
+pixels.fill((255, 255, 255))
